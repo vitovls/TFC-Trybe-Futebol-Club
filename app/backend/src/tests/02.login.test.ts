5de9fbeeb2f303552169d9f0e7e073530239f944
vitovls <vitovls37@gmail.com>
2022-03-24 01:03:21 -0300

    feat: impl tests to /login route returns

---
+++ app/backend/src/tests/02.login.test.ts
@@ -0,0 +1,69 @@
+import * as sinon from 'sinon';
+import * as chai from 'chai';
+import chaiHttp = require('chai-http');
+
+import { app } from '../app';
+import Users from '../database/models/Users';
+
+import { reqBody } from './reqBody'
+
+const { validLogin, noEmail,
+  noPassword, errorMail1,
+  errorMail2, errorMail3,
+  errorPassword } = reqBody
+
+import { Response } from 'superagent';
+
+import {findOneMock} from '../tests/mocks/loginMocks'
+import { generateToken } from '../helpers/token';
+
+chai.use(chaiHttp);
+
+const { expect } = chai;
+
+describe('Verifica o retorno da rota "/login"', () => {
+  let res: Response;
+  let result: boolean;
+
+  before(async () => {
+    sinon
+      .stub(Users, 'findOne')
+      .resolves(findOneMock as Users);
+  });
+
+  after(()=>{
+    (Users.findOne as sinon.SinonStub).restore()
+  })
+
+  it('Se o login for bem sucedido retorna um objeto com o token', async () => {
+    res = await chai.request(app).post('/login').send(validLogin)
+    const {email} = validLogin
+    const token = generateToken(email)
+    result = res.text.includes(token)
+    expect(res).to.be.a('object');
+    expect(result).to.be.eq(true);
+  });
+  it('Se a requisição vinher sem email e/ou senha, retona um erro', async () => {
+    res = await chai.request(app).post('/login').send(noEmail);
+    result = res.text.includes('All fields must be filled');
+    expect(result).to.eq(true);
+    res = await chai.request(app).post('/login').send(noPassword);
+    expect(result).to.eq(true);
+  })
+  it('Se a requisição vinher com email inválido, retona um erro', async () => {
+    res = await chai.request(app).post('/login').send(errorMail1);
+    result = res.text.includes('Incorrect email or password');
+    expect(result).to.eq(true);
+    res = await chai.request(app).post('/login').send(errorMail2);
+    result = res.text.includes('Incorrect email or password');
+    expect(result).to.eq(true);
+    res = await chai.request(app).post('/login').send(errorMail3);
+    result = res.text.includes('Incorrect email or password')
+    expect(result).to.eq(true);
+  })
+  it('Se a senha tiver menos de 6 caracteres, retorna um erro', async () => {
+    res = await chai.request(app).post('/login').send(errorPassword);
+    result = res.text.includes('Incorrect email or password');
+    expect(result).to.eq(true);
+  })
+});
