3a451b9fd94f7b90be1d017eac25c84808ade20d
vitovls <vitovls37@gmail.com>
2022-03-24 18:03:11 -0300

    feat: loginServices to return role and impl password verification

--- app/backend/src/services/loginServices.ts
+++ app/backend/src/services/loginServices.ts
@@ -1,20 +1,37 @@
-import { generateToken } from '../helpers/token';
+import * as bcrypt from 'bcrypt';
+import { generateToken, verifyToken } from '../helpers/token';
 import Users from '../database/models/Users';
 
-const getUserByMail = async (mail: string) => {
+const getUserByMail = async (mail: string, pass: string) => {
   const user = await Users.findOne({ where: { email: mail } });
   const token = generateToken(mail);
   const NOT_FOUND = 'NOT FOUND';
   if (user !== null) {
-    return {
-      user,
-      token,
-    };
+    const validPassword = await bcrypt.compare(pass, user?.password);
+    if (validPassword) {
+      const { id, username, role, email } = user;
+      return {
+        user: {
+          id,
+          username,
+          role,
+          email,
+        },
+        token,
+      };
+    }
   } return NOT_FOUND;
+};
+
+const getRole = async (auth:string) => {
+  const validToken = await verifyToken(auth);
+  const user = await Users.findOne({ where: { email: validToken } });
+  if (user !== null) return user.role;
 };
 
 const loginServices = {
   getUserByMail,
+  getRole,
 };
 
 export default loginServices;
