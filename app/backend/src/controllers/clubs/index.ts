a71fcba1f1225214769a6790b946054b068c03a2
vitovls <vitovls37@gmail.com>
2022-03-27 19:34:05 -0300

    feat: impl clubs controller get clubs by id in service

--- app/backend/src/controllers/clubs/index.ts
+++ app/backend/src/controllers/clubs/index.ts
@@ -1,10 +1,14 @@
 import { Request, Response } from 'express';
 import StatusCodes from '../../helpers/StatusCode';
-import clubsServices from '../../services/clubs/index';
+import { getAllClubs, getClubsById } from '../../services/clubs/index';
 
-const clubsController = async (req:Request, res:Response) => {
-  const listClubs = await clubsServices.getAllClubs();
+export const clubsGetAll = async (req:Request, res:Response) => {
+  const listClubs = await getAllClubs();
   return res.status(StatusCodes.OK).json(listClubs);
 };
 
-export default clubsController;
+export const clubsGetById = async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const club = await getClubsById(id);
+  return res.status(StatusCodes.OK).json(club);
+};
