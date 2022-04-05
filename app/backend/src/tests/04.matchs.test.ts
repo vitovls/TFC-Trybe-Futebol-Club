e5ec521c3b137f133cab1a5677a7635b2d71923b
vitovls <vitovls37@gmail.com>
2022-04-05 01:38:08 -0300

    test: impl tests to status /matchs

--- app/backend/src/tests/04.matchs.test.ts
+++ app/backend/src/tests/04.matchs.test.ts
@@ -107,25 +107,26 @@ describe('Testes rota "/matchs"', () => {
       expect(res.text).to.be.eq(resolve);
     });
   })
-  // describe('METHOD PATCH', () => {
-  //   afterEach(() => {
-  //     (Matchs.findOne as sinon.SinonStub).restore();
-  //   })
-  //   it('"/matchs/:id" com o corpo da requisição valido, retorna um status 200, com o partida atualizada', async () => {
-  //     sinon.stub(Matchs, 'findOne').resolves(updatedMatchReturnMock as Matchs);
+  describe('METHOD PATCH', () => {
+    afterEach(() => {
+      (Matchs.findOne as sinon.SinonStub).restore();
+    })
+    it('"/matchs/:id" com o corpo da requisição valido, retorna um status 200, com o partida atualizada', async () => {
+      sinon.stub(Matchs, 'findOne').resolves(updatedMatchReturnMock as Matchs);
 
-  //     res = await chai.request(app)
-  //       .patch('/matchs/47').send(updateMatchPayload);
-  //     resolve = JSON.stringify(updatedMatchReturnMock);
-  //     expect(res.text).to.be.eq(resolve);
-  //   });
-  //   it('PATCH na rota "/matchs/:id/finish" com o corpo da requisição valido, retorna um status 200, com o partida atualizada', async () => {
-  //     sinon.stub(Matchs, 'findOne').resolves(finishMatchMock as Matchs);
+      res = await chai.request(app)
+        .patch('/matchs/47').send(updateMatchPayload);
+      
+      expect(res).to.have.a.status(StatusCodes.OK);
+    });
+    it('PATCH na rota "/matchs/:id/finish" com o corpo da requisição valido, retorna um status 200, com o partida atualizada', async () => {
+      sinon.stub(Matchs, 'findOne').resolves(finishMatchMock as Matchs);
+      
+      res = await chai.request(app)
+        .patch('/matchs/47/finish');
 
-  //     res = await chai.request(app)
-  //       .patch('/matchs/47/finish');
-  //     resolve = JSON.stringify(finishMatchMock);
-  //     expect(res.text).to.be.eq(resolve);
-  //   });
-  // })
+      expect(res).to.have.a.status(StatusCodes.OK);
+
+    });
+  })
 });
